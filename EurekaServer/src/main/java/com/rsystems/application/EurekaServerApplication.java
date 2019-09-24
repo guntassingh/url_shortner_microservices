3272b208a5955a63540c940e389dca0c14e5e0e1
Amit Maurya <Amit.Maurya@rsystems.com>
2019-09-24 14:20:01 +0530

    Tested with new DTO

--- EurekaServer/src/main/java/com/rsystems/application/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/rsystems/application/EurekaServerApplication.java
@@ -12,9 +12,7 @@ public class EurekaServerApplication {
 	static Logger logger = LoggerFactory.getLogger(EurekaServerApplication.class);
 
 	public static void main(String[] args) {
-		logger.info("this is test message");
-		logger.debug("this is test message");
-		logger.error("this is test message");
+
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
